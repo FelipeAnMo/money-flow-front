8b7697a38103934f45e9225f861174468771bfdd
Felipe Moretzsohn <felipe.moretzsohn@gmail.com>
2025-03-16 17:18:58 -0300

    Added more elements to navbar

--- src/components/NavegationHeader/NavegationHeader.jsx
+++ src/components/NavegationHeader/NavegationHeader.jsx
@@ -3,14 +3,44 @@ import './NavegationHeader.css';
 function NavegationHeader() {
     return (
         <nav>
+            <div id='box-top-menu'>
+                <button type='button' id='btn-hidden-show-nav'>
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+            </div>
             <ul>
                 <li>
                     <a href="#">
-                        <i class="fa-solid fa-house"></i>
-                        Dashboard
+                        <div>
+                            <i class="fa-solid fa-house"></i>
+                            Dashboard
+                        </div>
                     </a>
                 </li>
-                <li></li>
+                <li>
+                    <a href="#">
+                        <div>
+                            <i class="fa-solid fa-calendar"></i>
+                            Calendário
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div>
+                            <i class="fa-solid fa-filter"></i>
+                            CRM
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div>
+                            <i class="fa-solid fa-coins"></i>
+                            Financeiro
+                        </div>
+                    </a>
+                </li>
             </ul>
         </nav>
     );
